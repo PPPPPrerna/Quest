13cd40fa786fd1202647c8e6afb7aaf6fa5dc8c1
PPPPPrerna <pandeyprerna248@gmail.com>
2020-09-12 14:05:12 +0530

    First

---
+++ src/Ae.java
@@ -0,0 +1,21 @@
+
+public class Ae {
+	
+	
+	public void test() {
+		System.out.println("Yeah");
+		
+		//final  up() {
+			
+		//}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+
+		}
+	}
+
+
